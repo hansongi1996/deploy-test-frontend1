f154bb915db5be0ffe381a2570cea63d24d8d99d
hansongi1996 <hansongi1996@gmail.com>
2025-11-26 15:01:43 +0900

    Setup deploy test with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/deploy-test-frontend1/',
   plugins: [react()],
   define: {
     global: 'window',        // ← 핵심
