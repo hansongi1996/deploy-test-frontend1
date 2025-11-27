4e9c85e487d522fc06828dca0a3ff7790b4ac176
hansongi1996 <hansongi1996@gmail.com>
2025-11-27 17:24:38 +0900

    fix: base path for nginx

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/deploy-test-frontend1/',
+  base: '/',
   plugins: [react()],
   define: {
     global: 'window',        // ← 핵심
